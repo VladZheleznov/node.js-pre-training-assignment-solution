53d995e2b8df921a62c248d3914e96d979834ada
Vlad Zheleznov <kryt2630@gmail.com>
2026-07-18 20:18:59 +0300

    feat: define a Todo factory with unique IDs

--- JS-TS/solutions/todo-factory.ts
+++ JS-TS/solutions/todo-factory.ts
@@ -3,5 +3,12 @@ import { Todo, NewTodo, TodoStatus } from './types';
 let nextId = 1;
 
 export function createTodo(input: NewTodo): Todo {
-  throw new Error('createTodo: not implemented');
+  const newTodo: Todo = {
+    id: nextId++,
+    title: input.title,
+    description: input.description,
+    status: input.status ?? TodoStatus.PENDING,
+    createdAt: new Date(),
+  };
+  return newTodo;
 }
