4ad71c93cddf04e8d4d29bcd6f2eac5474bae799
Vlad Zheleznov <kryt2630@gmail.com>
2026-07-20 11:10:17 +0300

    feat: define core CRUD functions

--- JS-TS/solutions/todo-crud.ts
+++ JS-TS/solutions/todo-crud.ts
@@ -1,17 +1,30 @@
 import { Todo } from './types';
 
 export function addTodo(state: Todo[], todo: Todo): Todo[] {
-  throw new Error('addTodo: not implemented');
+  return [...state, todo];
 }
 
-export function updateTodo(state: Todo[], id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Todo[] {
-  throw new Error('updateTodo: not implemented');
+export function updateTodo(
+  state: Todo[],
+  id: number,
+  update: Partial<Omit<Todo, 'id' | 'createdAt'>>
+): Todo[] {
+  if (!state.some((todo) => todo.id === id)) {
+    throw new Error(`Todo with id ${id} not found`);
+  }
+  return state.map((todo) => (todo.id === id ? { ...todo, ...update } : todo));
 }
 
 export function removeTodo(state: Todo[], id: number): Todo[] {
-  throw new Error('removeTodo: not implemented');
+  if (!state.some((todo) => todo.id === id)) {
+    throw new Error(`Todo with id ${id} not found`);
+  }
+  return state.filter((todo) => todo.id !== id);
 }
 
 export function getTodo(state: Todo[], id: number): Todo | undefined {
-  throw new Error('getTodo: not implemented');
+  if (!state.some((todo) => todo.id === id)) {
+    throw new Error(`Todo with id ${id} not found`);
+  }
+  return state.find((todo) => todo.id === id);
 }
