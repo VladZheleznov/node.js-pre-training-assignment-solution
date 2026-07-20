08c9cc9c62e1efe0ea5fad81b19ac18b33e5fa3a
Vlad Zheleznov <kryt2630@gmail.com>
2026-07-21 00:08:22 +0300

    define an asynchronous wrapper

--- JS-TS/solutions/todo-api.ts
+++ JS-TS/solutions/todo-api.ts
@@ -1,22 +1,45 @@
 import { InMemoryRepository } from './repository';
 import { Todo, NewTodo } from './types';
 
+export class TodoNotFoundError extends Error {
+  constructor(id: number) {
+    super(`Todo with id ${id} not found`);
+  }
+}
+
 export class TodoApi {
   private repo = new InMemoryRepository<Todo>();
 
   async getAll(): Promise<Todo[]> {
-    throw new Error('getAll: not implemented');
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return this.repo.findAll();
   }
 
   async add(newTodo: NewTodo): Promise<Todo> {
-    throw new Error('add: not implemented');
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    return this.repo.add({
+      ...newTodo,
+      id: Date.now(),
+      createdAt: new Date(),
+    });
   }
 
-  async update(id: number, update: Partial<Omit<Todo, 'id' | 'createdAt'>>): Promise<Todo> {
-    throw new Error('update: not implemented');
+  async update(
+    id: number,
+    update: Partial<Omit<Todo, 'id' | 'createdAt'>>
+  ): Promise<Todo> {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    if (!this.repo.findById(id)) {
+      throw new TodoNotFoundError(id);
+    }
+    return this.repo.update(id, update);
   }
 
   async remove(id: number): Promise<void> {
-    throw new Error('remove: not implemented');
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    if (!this.repo.findById(id)) {
+      throw new TodoNotFoundError(id);
+    }
+    this.repo.remove(id);
   }
 }
